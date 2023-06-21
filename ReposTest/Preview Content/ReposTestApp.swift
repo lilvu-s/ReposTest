8f649a54b376ed1d2f5bab32aed75e16124bf6ae
Angelina <anhelina.semenchenko@gmail.com>
2023-06-21 04:52:08 +0300

    Setup base layers

---
+++ ReposTest/Preview Content/ReposTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReposTestApp.swift
+//  ReposTest
+//
+//  Created by Ангеліна Семенченко on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReposTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
